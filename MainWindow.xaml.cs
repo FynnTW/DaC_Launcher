8c409f1ad99fddb45b017b4b57c3e1a9486a3468
Eddie Eldridge <steadyeddie101@hotmail.co.uk>
2023-06-30 16:04:52 +0100

    Update Window styling

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,7 +27,6 @@ namespace DaC_Launcher
             {
                 MessageBox.Show($"Start not possible or Exception occurs {e}  ", "DaC Launcher Error");
             }
-
         }
 
         private void LoadSettings()
@@ -317,6 +316,13 @@ namespace DaC_Launcher
                 const bool overwriteFiles = true;
                 File.Copy(newPath, newPath.Replace(sourceDir, destinationDir), overwriteFiles);
             }
+        }
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // for .NET Core you need to add UseShellExecute = true
+            // see https://learn.microsoft.com/dotnet/api/system.diagnostics.processstartinfo.useshellexecute#property-value
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
         }
     }
 }
